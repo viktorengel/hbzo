73600bedf418d6e7cb5fd4e45d214d07d9f480a5
viktorengel <viktorengel@hotmail.com>
2022-03-05 07:20:28 -0500

    Horarios

--- database/seeders/WorkDaysTableSeeder.php
+++ database/seeders/WorkDaysTableSeeder.php
@@ -19,11 +19,11 @@ class WorkDaysTableSeeder extends Seeder
         		'day' => $i,
 		        'active' => ($i==0), // Thursday
 
-		        'morning_start' => ($i==0 ? '07:00:00' : '05:00:00'),
-		        'morning_end' => ($i==0 ? '09:30:00' : '05:00:00'),
+		        'morning_start' => ($i==0 ? '09:00:00' : '09:00:00'),
+		        'morning_end' => ($i==0 ? '11:30:00' : '11:30:00'),
 
-		        'afternoon_start' => ($i==0 ? '15:00:00' : '13:00:00'),
-		        'afternoon_end' => ($i==0 ? '18:00:00' : '13:00:00'),
+		        'afternoon_start' => ($i==0 ? '16:00:00' : '16:00:00'),
+		        'afternoon_end' => ($i==0 ? '18:00:00' : '18:00:00'),
 
 		        'user_id' => 3 // Médico Test (UsersTableSeeder)
         	]);
